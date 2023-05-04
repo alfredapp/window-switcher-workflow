8a2415506043248d7012a9748952d34ef259360c
Vítor Galvão <info@vitorgalvao.com>
2023-05-04 19:07:30 +0100

    get-windows.swift: Use struct and comment

--- Resources/get-windows.swift
+++ Resources/get-windows.swift
@@ -2,12 +2,23 @@
 
 import AppKit
 
+// Helpers
+struct ScriptFilterItem: Codable {
+  let title: String
+  let subtitle: String
+  let arg: [Int32]
+  let icon: [String: String]
+  let match: String
+}
+
+// Grab windows
 let windowList: CFArray? = CGWindowListCopyWindowInfo(
   [.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID)
 
 guard let windows = windowList as? [[String: Any]] else { fatalError("Unable to get window list") }
 
-let sfItems = windows.compactMap { (dict: [String: Any]) -> [String: Any]? in
+// Populate items
+let sfItems: [ScriptFilterItem] = windows.compactMap { dict in
   guard
     dict["kCGWindowLayer"] as? Int == 0,
     let appName = dict["kCGWindowOwnerName"] as? String,
@@ -19,34 +30,31 @@ let sfItems = windows.compactMap { (dict: [String: Any]) -> [String: Any]? in
 
   let windowName = windowTitle.isEmpty ? "Unnamed" : windowTitle
 
-  return [
-    "title": windowName,
-    "subtitle": appName,
-    "arg": [appPID, windowID],
-    "icon": ["type": "fileicon", "path": appPath],
-    "match": "\(windowName) \(appName)"
-  ]
+  return ScriptFilterItem(
+    title: windowName,
+    subtitle: appName,
+    arg: [appPID, windowID],
+    icon: ["type": "fileicon", "path": appPath],
+    match: "\(windowName) \(appName)"
+  )
 }
 
-if sfItems.isEmpty {
-  let alfredObject = [
-    "items": [
-      [
-        "title": "No Windows Found",
-        "subtitle":
-          "Make sure Alfred has Screen Recording permissions. Press ↩ to open System Settings.",
-        "arg": "request_permissions"
-      ]
-    ]
+// Fallback if no valid items
+guard !sfItems.isEmpty else {
+  let notFound = [
+    "title": "No Windows Found",
+    "subtitle":
+      "Make sure Alfred has Screen Recording permissions. Press ↩ to open System Settings.",
+    "arg": "request_permissions"
   ]
 
-  let jsonData: Data = try JSONSerialization.data(withJSONObject: alfredObject)
+  let jsonData: Data = try JSONSerialization.data(withJSONObject: ["items": [notFound]])
   let jsonString: String = String(data: jsonData, encoding: .utf8)!
 
   print(jsonString)
   exit(0)
 }
 
-let jsonData: Data = try JSONSerialization.data(withJSONObject: ["items": sfItems])
-let jsonString: String = String(data: jsonData, encoding: .utf8)!
-print(jsonString)
+// Output JSON
+let jsonData = try JSONEncoder().encode(["items": sfItems])
+print(String(data: jsonData, encoding: .utf8)!)
